4ff2956ebd69c0ca5e3e4039d04a468ca52b4107
El-Glory <elgloryoriabure@gmail.com>
2020-11-24 23:42:16 +0100

    Updates

---
+++ precache-manifest.546abd125fb43dafb6dae47e3301966b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74e7bd04079be1560f24f51dae4d3535",
+    "url": "/El-Glory/React-calculator.git/index.html"
+  },
+  {
+    "revision": "bdd91e47c6224d006abe",
+    "url": "/El-Glory/React-calculator.git/static/css/main.7c6b3edf.chunk.css"
+  },
+  {
+    "revision": "30488d54126ba964ca3b",
+    "url": "/El-Glory/React-calculator.git/static/js/2.4ed6764d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/El-Glory/React-calculator.git/static/js/2.4ed6764d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdd91e47c6224d006abe",
+    "url": "/El-Glory/React-calculator.git/static/js/main.91dbe170.chunk.js"
+  },
+  {
+    "revision": "6a471999cc0fac9b04aa",
+    "url": "/El-Glory/React-calculator.git/static/js/runtime-main.f557a5bd.js"
+  }
+]);
